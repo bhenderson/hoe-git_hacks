37033d4e6434de57f47bdc40a2cf2d092f2d793c
bhenderson <bhenderson@attinteractive.com>
2012-07-11 15:44:46 -0700

    Initial commit.

---
+++ Rakefile
@@ -0,0 +1,17 @@
+# -*- ruby -*-
+
+require 'rubygems'
+require 'hoe'
+
+Hoe.plugin :atti_push
+Hoe.plugin :git
+
+Hoe.spec 'hoe-git_hacks' do
+  developer 'Brian Henderson', 'bhenderson@attinteractive.com'
+
+  self.readme_file      = "README.rdoc"
+
+  self.testlib          = :minitest
+end
+
+# vim: syntax=ruby
---
+++ lib/hoe/git_hacks.rb
@@ -0,0 +1,17 @@
+class Hoe
+  module Git_Hacks
+    VERSION = '1.0.0'
+
+    # https://github.com/jbarnette/hoe-git/pull/8
+    def initialize_git_hacks
+      task(:release_to).prerequisites.delete 'git:tag'
+      task :postrelease => 'git:tag'
+    end
+
+    # https://github.com/jbarnette/hoe-git/pull/7
+    def git_tags
+      flags = "--date-order --simplify-by-decoration --pretty=format:%d"
+      `git log #{flags}`.scan(%r{#{git_release_tag_prefix}[^,)]+}).reverse
+    end
+  end if Hoe.plugins.include?(:git)
+end
